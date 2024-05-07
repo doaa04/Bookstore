9b2c29ff98c25118a81202c2a35732848d85fc1c
doaa04 <doaarigui0@gmail.com>
2024-05-07 22:32:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.use(express.static('public'));
 
 const mongodb = require('mongodb'); 
 const mongoose = require('mongoose');
-const uriJ = "mongodb+srv://root:root@cluster0.djhput1.mongodb.net/database0?retryWrites=true&w=majority&appName=Cluster0";
 const uri = "mongodb://localhost:27017/database0";
 
 
